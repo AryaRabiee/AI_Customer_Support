fd00ae4c61615a450518caf91fc125a29bfd40f2
Arya-Rabiee <arya.r1384@gmail.com>
2026-07-22 19:49:56 +0330

    update main and add tempaltes

--- app/main.py
+++ app/main.py
@@ -1,11 +1,25 @@
 from fastapi import FastAPI
 from api.routes.fag import router as fag_router
 from dotenv import load_dotenv
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import FileResponse
+from pathlib import Path
+
+file_path = Path(__file__).parent.parent
 
 load_dotenv()
 app = FastAPI(
     title="AI Customer Support Agent",
     version="0.1.0",
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(
@@ -19,4 +33,11 @@ async def health_check():
         "status": "ok"
     }
 
+@app.get("/")
+async def root():
+    return FileResponse(file_path/"templates/home.html")
+
+@app.get("/fag")
+async def support_page():
+    return FileResponse(file_path/"templates/faq.html")
 
