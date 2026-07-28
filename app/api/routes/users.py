053d1882c40be22392e9a1716fa861973e0bfbcd
Arya-Rabiee <arya.r1384@gmail.com>
2026-07-28 15:22:48 +0330

    feat:add Authentication

---
+++ app/api/routes/users.py
@@ -0,0 +1,22 @@
+from fastapi import APIRouter, Depends
+
+from api.dependencies.auth import get_current_user
+from db.models import User
+
+
+router = APIRouter(
+    prefix="/users",
+    tags=["Users"]
+)
+
+
+@router.get("/me")
+def get_me(
+    current_user: User = Depends(get_current_user)
+):
+
+    return {
+        "user_id": current_user.user_id,
+        "name": current_user.name,
+        "email": current_user.email
+    }
