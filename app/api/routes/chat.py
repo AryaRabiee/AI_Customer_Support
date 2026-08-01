505ed56bb1fd1b423bf5f1714812cff12a7b368c
Arya-Rabiee <arya.r1384@gmail.com>
2026-08-01 22:58:34 +0330

    feat : add chat function

---
+++ app/api/routes/chat.py
@@ -0,0 +1,25 @@
+from schemas.chat import ChatMessage
+from fastapi import APIRouter ,status , HTTPException , Depends
+from api.dependencies.auth import get_current_user
+from graph.support_graph import run_support_agent
+
+
+router = APIRouter(
+    prefix="/chat",
+    tags=["Chat"]
+)
+@router.post("/message")
+async def chat_message( data: ChatMessage,current_user = Depends(get_current_user)):
+    
+    user_message = data.message.strip()
+    print(user_message)
+    if not user_message:   
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="پیام خالی است"
+        )
+    response = run_support_agent(user_message)
+    return {    
+        "response": response,
+        "status": "success"
+    }
