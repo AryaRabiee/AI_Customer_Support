a13e3913173d9d949e61e103d3a53730505dcdbe
Arya-Rabiee <arya.r1384@gmail.com>
2026-08-01 22:54:32 +0330

    feat : Add state

---
+++ app/graph/state.py
@@ -0,0 +1,17 @@
+from typing import Literal
+from typing_extensions import TypedDict
+from pydantic import BaseModel, Field 
+
+class SupportState(TypedDict):
+    user_message: str
+    next_agent: Literal["chat", "rag", "database"]
+    response : str
+
+class SupervisorDecision(BaseModel):
+    next_agent: Literal["chat", "rag", "database"] = Field(
+        description="The agent that should handle the request"
+    )
+
+class ExtractData(BaseModel):
+    intent : Literal["order_status" , "order_details","cancel_order","return_order"]
+    order_id: int | None = None
