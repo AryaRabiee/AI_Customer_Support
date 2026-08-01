ac6724dfa3fcaae45558944997498ba425b7448f
Arya-Rabiee <arya.r1384@gmail.com>
2026-08-01 22:57:45 +0330

    feat : add chat_node

---
+++ app/agents/chat_agent.py
@@ -0,0 +1,30 @@
+from langchain_openrouter import ChatOpenRouter
+from graph.state import SupportState
+from langchain.messages import HumanMessage , SystemMessage
+from utils.prompts import CHAT_PROMPT
+
+import os
+
+api_key = os.getenv("EMBEDDING_API_KEY")
+
+model = ChatOpenRouter(
+    model="openai/gpt-oss-20b:free",
+    api_key=api_key,
+    temperature=0.5
+)
+
+
+
+def chat_node(state : SupportState):
+
+    message = state["user_message"]
+
+    response = model.invoke([
+        SystemMessage(content = CHAT_PROMPT),
+        HumanMessage(content = message)
+    ])
+
+    return {
+    "response": response.content
+    }
+
