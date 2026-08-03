78a4284a76109a26513901d061d38580b1925b69
Arya-Rabiee <arya.r1384@gmail.com>
2026-08-03 22:18:38 +0330

    update chat_agent

--- app/agents/chat_agent.py
+++ app/agents/chat_agent.py
@@ -2,6 +2,7 @@ from langchain_openrouter import ChatOpenRouter
 from graph.state import SupportState
 from langchain.messages import HumanMessage , SystemMessage
 from utils.prompts import CHAT_PROMPT
+from langchain_core.messages import AIMessage
 
 import os
 
@@ -9,22 +10,22 @@ api_key = os.getenv("EMBEDDING_API_KEY")
 
 model = ChatOpenRouter(
     model="openai/gpt-oss-20b:free",
-    api_key=api_key,
+    api_key="***REMOVED***",
     temperature=0.5
 )
 
 
 
-def chat_node(state : SupportState):
+def chat_node(state: SupportState):
 
     message = state["user_message"]
 
     response = model.invoke([
-        SystemMessage(content = CHAT_PROMPT),
-        HumanMessage(content = message)
+        SystemMessage(content=CHAT_PROMPT),
+        HumanMessage(content=message)
     ])
 
     return {
-    "response": response.content
+        "response": response.content
     }
 
