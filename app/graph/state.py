20019444eff933b718c150f06181849923869034
Arya-Rabiee <arya.r1384@gmail.com>
2026-08-12 16:09:52 +0330

    feat : Add RefundOutput and update supportstate

--- app/graph/state.py
+++ app/graph/state.py
@@ -4,24 +4,76 @@ from pydantic import BaseModel, Field
 from langgraph.graph import MessagesState
 
 class SupportState(MessagesState):
-    user_id : str
+    user_id: str
     user_message: str
-    next_agent: Literal["chat", "rag", "database"]
+    next_agent: Literal["chat", "rag", "database", "refund"]
     response: str
 
     intent: Literal[
         "order_status",
         "order_details",
-        "cancel_order",
+        "refund_order",
         "return_order"
     ] | None
-    order_id: int | None
 
+    order_id: int | None
+    product_id: int | None
+
+    refund_action: Literal[
+        "ASK_REASON",
+        "ASK_INFORMATION",
+        "CHECK_DATABASE",
+        "FINAL"
+    ] | None
+
+    reason: Literal[
+        "wrong_product",
+        "damaged_product",
+        "technical_problem",
+        "changed_mind",
+        "not_as_described",
+        "other"
+    ] | None
+
+    description: str | None
+
+    decision: Literal[
+        "APPROVE",
+        "REJECT",
+        "REVIEW"
+    ] | None
 class SupervisorDecision(BaseModel):
     next_agent: Literal["chat", "rag", "database"] = Field(
         description="The agent that should handle the request"
     )
 
 class ExtractData(BaseModel):
-    intent : Literal["order_status" , "order_details","cancel_order","return_order"]
+    intent : Literal["order_status" , "order_details","return_order"]
     order_id: int | None = None
+
+class RefundOutput(BaseModel):
+    action: Literal[
+        "ASK_REASON",
+        "ASK_INFORMATION",
+        "CHECK_DATABASE",
+        "FINAL"
+    ]
+
+    reason: Literal[
+        "wrong_product",
+        "damaged_product",
+        "technical_problem",
+        "changed_mind",
+        "not_as_described",
+        "other"
+    ] | None = None
+
+    order_id: int | None = None
+    product_id: int | None = None
+    expected_product: str | None = None  
+    received_product: str | None = None 
+    description: str | None = None
+    message: str
+
+
+    
