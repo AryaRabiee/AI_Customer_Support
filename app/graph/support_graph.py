d222877734206ad8125f50daad23dff134720903
Arya-Rabiee <arya.r1384@gmail.com>
2026-08-12 16:12:41 +0330

    update support_graph

--- app/graph/support_graph.py
+++ app/graph/support_graph.py
@@ -8,6 +8,7 @@ from agents.order_status_agent import order_status_node
 from agents.order_detail import order_detail_node
 from langchain_core.messages import HumanMessage
 from langgraph.checkpoint.memory import InMemorySaver
+from agents.refund_agent import refund_agent
 
 
 
@@ -20,7 +21,7 @@ graph.add_node("chat", chat_node)
 graph.add_node("database", extract_data)
 graph.add_node("order_status", order_status_node)
 graph.add_node("order_details", order_detail_node)
-
+graph.add_node("refund", refund_agent)
 
 graph.add_edge(START, "supervisor")
 
@@ -31,7 +32,8 @@ graph.add_conditional_edges(
     {
         "rag": "rag",
         "chat": "chat",
-        "database": "database"
+        "database": "database",
+        "refund":"refund",
     }
 )
 
@@ -41,7 +43,7 @@ graph.add_conditional_edges(
     lambda state: state["intent"],
     {
         "order_status": "order_status",
-        "order_details": "order_details"
+        "order_details": "order_details",
     }
 )
 
@@ -50,7 +52,7 @@ graph.add_edge("rag", END)
 graph.add_edge("chat", END)
 graph.add_edge("order_status", END)
 graph.add_edge("order_details", END)
-
+graph.add_edge("refund", END)
 
 app = graph.compile(
     checkpointer=memory
@@ -73,7 +75,12 @@ def run_support_agent(user_message: str,user_id: int , thread_id):
             }
         }
     )
-
+    print("===== STATE بعد از این پاس =====")
+    print("intent:", result.get("intent"))
+    print("next_agent:", result.get("next_agent"))
+    print("order_id:", result.get("order_id"))
+    print("response:", result.get("response"))
+    print("="*40)
     print("result:", result)
 
     return result["response"]
