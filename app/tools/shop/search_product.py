9d1e06be1f2717da1b52ebfbfd462f74951738cc
Arya-Rabiee <arya.r1384@gmail.com>
2026-08-30 23:01:02 +0330

    feat: add web search fallback for shop agent

---
+++ app/tools/shop/search_product.py
@@ -0,0 +1,22 @@
+from langchain.tools import tool
+from utils.logger import logging
+import requests
+import os
+
+api_key = os.getenv("t_search")
+logger = logging.getLogger(__name__)
+
+@tool
+def search_product(product):
+    """ this tool is going to search about the product from internt """
+    logger.info("tool calling search_product")
+    response = requests.post(
+        "https://api.avalai.ir/v1/search/tavily-search",
+        headers={"Authorization": f"Bearer {api_key}", "Content-Type": "application/json"},
+        json={"query":f"اطلاعاتی درباره {product}", "max_results": 5 , "country":"iran"},
+    )
+    result = response.json()
+
+    return {
+        "response":result
+    }
