6cb186f2255f747c01f34d3a835aac7c358d58f5
Arya-Rabiee <arya.r1384@gmail.com>
2026-08-30 23:00:06 +0330

    feat: add rag system  shop_agent

---
+++ app/tools/shop/products_informations.py
@@ -0,0 +1,37 @@
+from langchain.tools import tool
+from utils.logger import logging
+from .hybrid_search_products import hybrid_search_products
+import os
+from pathlib import Path
+from bm25s import BM25
+from hazm import Normalizer , word_tokenize
+from langchain_chroma import Chroma
+from langchain_openai import OpenAIEmbeddings , ChatOpenAI
+from graph.state import SupportState
+from langgraph.prebuilt import InjectedState
+from typing import Annotated
+from dotenv import load_dotenv
+current_dir = Path(__file__).parent
+CHROMA_DIR = current_dir / "chroma_db"
+
+logger = logging.getLogger(__name__)
+
+
+
+@tool
+def product_info_search(
+    products,
+    user_message: Annotated[str, InjectedState("user_message")]
+):
+    """Give detailed information about the products found for the customer."""
+
+    logger.info("Calling tool products_informations")
+    print(products)
+    result = hybrid_search_products(
+        message=user_message,
+        products=products
+    )
+
+    return {
+        "response": result
+    }
