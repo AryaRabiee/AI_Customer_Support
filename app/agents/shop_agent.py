69842364915ec78f99ad833956698b60244896bc
Arya-Rabiee <arya.r1384@gmail.com>
2026-08-30 23:04:02 +0330

    feat:Add new agent (shop_agent)

---
+++ app/agents/shop_agent.py
@@ -0,0 +1,40 @@
+from graph.state import SupportState
+from utils.logger import logging
+from utils.call_llm import call_llm_with_tools
+import os
+from langchain_openai import ChatOpenAI
+from langchain.messages import SystemMessage , AIMessage
+from utils.prompts import SHOP_AGENT_PRONPT
+from tools.shop.find_product import shop_find_product
+from tools.shop.products_informations import product_info_search
+from tools.shop.search_product import search_product
+logger = logging.getLogger(__name__)
+
+api_key = os.getenv("GPT_API_KEY")
+model = os.getenv("MODEL")
+base_url = os.getenv("BASE_URL_GAP")
+model = ChatOpenAI(
+    model=model,
+    api_key=api_key,
+    base_url=base_url
+)
+
+def shop_agent(state:SupportState):
+    logger.info("START FUNC SHOP AGENT")
+
+    result = call_llm_with_tools(
+        model,
+        [
+            SystemMessage(content=SHOP_AGENT_PRONPT),
+            *state["messages"]
+
+        ],
+        [shop_find_product,product_info_search,search_product]
+
+    )
+    return {
+        "response":result.content,
+        "messages":[
+            AIMessage(content=result.content , tool_calls = result.tool_calls)
+        ]
+    }
