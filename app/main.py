2ddedf658a76f1e49f0a5bfd5ec1f793182add2f
Arya-Rabiee <arya.r1384@gmail.com>
2026-09-06 23:17:45 +0330

    update main and add init

--- app/main.py
+++ app/main.py
@@ -1,22 +1,47 @@
 from fastapi import FastAPI
-from api.routes.faq import router as fag_router
-from api.routes.auth import router as auth_router
-from api.routes.users import router as users_router
-from api.routes.chat import router as chat_router
+from app.api.routes.faq import router as fag_router
+from app.api.routes.auth import router as auth_router
+from app.api.routes.users import router as users_router
+from app.api.routes.chat import router as chat_router
+from fastapi.requests import Request
+from jose import jwt
+from app.utils.security import decode_access_token
+from app.api.routes.chat import limiter
 from dotenv import load_dotenv
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import FileResponse
 from pathlib import Path
-import utils.logger
+from app.utils.logger import logging
+from slowapi import Limiter  , _rate_limit_exceeded_handler
+from slowapi.util import get_remote_address
+from slowapi.errors import RateLimitExceeded
+import os
+
+logger = logging.getLogger(__name__) 
+load_dotenv()
+SECRET_KEY = os.getenv("SECRET_KEY")
 file_path = Path(__file__).parent.parent
 
-load_dotenv()
 app = FastAPI(
     title="AI Customer Support Agent",
     version="0.1.0",
 )
-
+app.state.limiter = limiter
+app.add_exception_handler(RateLimitExceeded , _rate_limit_exceeded_handler)
+@app.middleware("http")
+async def set_user_in_state(request: Request, call_next):
+    token = request.cookies.get("access_token")
+    if token:
+        try:
+            payload = decode_access_token(token)
+            request.state.user_id = payload.get("sub")
+        except Exception as e:
+            logger.exception("ERROR AT MIDDELWARE FUNC" ,e)
+            raise
+    
+    response = await call_next(request)
+    return response
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],  
@@ -51,15 +76,6 @@ async def health_check():
 @app.get("/")
 async def home():
     return FileResponse(file_path/"templates/home.html")
-
-@app.get("/faq")
-async def faq_page():
-    return FileResponse(file_path/"templates/faq.html")
-
-
-@app.get("/test")
-async def test_page():
-    return FileResponse(file_path/"templates/test_home.html")
 
 @app.get("/login")
 async def login_page():
