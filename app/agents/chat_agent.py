7cec8623222f97d827560dae7688827a4fb139c1
Arya-Rabiee <arya.r1384@gmail.com>
2026-09-06 23:06:01 +0330

    update chat

--- app/agents/chat_agent.py
+++ app/agents/chat_agent.py
@@ -1,13 +1,11 @@
 from langchain_openrouter import ChatOpenRouter
-from graph.state import SupportState
+from app.graph.state import SupportState
 from langchain.messages import HumanMessage , SystemMessage
-from utils.prompts import CHAT_PROMPT
+from app.utils.prompts import CHAT_PROMPT
 from langchain_core.messages import AIMessage
 from langchain_openai import ChatOpenAI
-from exceptions.llm import LLMError , LLMTimeoutError
-from openai import APIConnectionError ,APITimeoutError
 import os
-from utils.call_llm import call_llm
+from app.utils.call_llm import call_llm
 from dotenv import load_dotenv
 import logging
 
@@ -18,7 +16,7 @@ api_key = os.getenv("QWEN_GAPGPT_KEY")
 base_url=os.getenv("BASE_URL_GAP")
 
 model = ChatOpenAI(
-    model="gapgpt-qwen-3.5",
+    model="gpt-5.6-luna",
     api_key=api_key,
     base_url=base_url,
     timeout=30,
@@ -40,4 +38,5 @@ def chat_node(state: SupportState):
             AIMessage(content=response.content)
         ]
     }
+
 
