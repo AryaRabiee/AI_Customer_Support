c42c6414057e95081e70146d75af219f48573661
Arya-Rabiee <arya.r1384@gmail.com>
2026-09-06 23:10:58 +0330

    update db

--- app/db/models.py
+++ app/db/models.py
@@ -1,7 +1,7 @@
 from sqlalchemy import Column, Integer, String, Boolean, DateTime , ForeignKey , Text , Numeric
 from sqlalchemy.sql import func
 
-from db.database import Base
+from app.db.database import Base
 
 
 class User(Base):
