3640bc3cf91556dc76f56e6330dffb95a0b23724
Arya-Rabiee <arya.r1384@gmail.com>
2026-09-06 23:07:45 +0330

    add limit for chat_message and add chat history function

--- app/api/routes/chat.py
+++ app/api/routes/chat.py
@@ -1,19 +1,33 @@
-from schemas.chat import ChatMessage
+from app.schemas.chat import ChatMessage
 from fastapi import APIRouter ,status , HTTPException , Depends
-from api.dependencies.auth import get_current_user
-from graph.support_graph import run_support_agent
-from db.models import User
-from exceptions.llm import LLMError , LLMTimeoutError
+from fastapi.requests import Request
+from fastapi.responses import StreamingResponse
+from app.api.dependencies.auth import get_current_user
+from app.graph.support_graph import run_support_agent , app
+from app.db.models import User
+from app.exceptions.llm import LLMError , LLMTimeoutError
 import logging
+import json
+from slowapi import Limiter
+from slowapi.util import get_remote_address
+from slowapi.errors import RateLimitExceeded
 
 logger = logging.getLogger(__name__)
+def get_user_rate_key(request: Request):
+    user_id = getattr(request.state, "user_id", None)
+    if user_id:
+        return f"user:{user_id}"
+    return get_remote_address(request)
+limiter = Limiter(key_func=get_user_rate_key)
 
 router = APIRouter(
     prefix="/chat",
     tags=["Chat"]
 )
+
 @router.post("/message")
-async def chat_message( data: ChatMessage,current_user:User = Depends(get_current_user)):
+@limiter.limit("14/minute")
+async def chat_message(request :Request , data: ChatMessage,current_user:User = Depends(get_current_user)):
     thread_id = current_user.user_id
     user_message = data.message.strip()
     logger.info(
@@ -34,8 +48,40 @@ async def chat_message( data: ChatMessage,current_user:User = Depends(get_curren
             "status": "success"
         }
 
-    except Exception:
+    except Exception as e:
+        logger.exception("ERROR FROM CHAT_MESSAGES FUNC %s",e )
         raise HTTPException(
             status_code=500,
             detail="Internal server error"
         )
+
+
+@router.get("/history")
+async def chat_history(
+    current_user: User = Depends(get_current_user)
+):
+    thread_id = current_user.user_id
+
+    try:
+        state = app.get_state({
+            "configurable": {"thread_id": thread_id}
+        })
+        
+        if not state or not state.values:
+            return {"messages": []}
+        
+        messages = state.values.get("messages", [])
+        
+        return {
+            "messages": [
+                {
+                    "role": message.type,
+                    "content": message.content
+                }
+                for message in messages
+                if message.type in ("human", "ai")
+            ]
+        }
+    except Exception as e:
+        logger.error(f"Error loading chat history: {e}")
+        return {"messages": []}  # خالی بجای خطا
